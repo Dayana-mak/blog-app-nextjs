8fcba05a631aafe0d5b2f68409e10499501b8ea1
Daiana Makushina <dayana.makushina@mail.ru>
2025-08-13 15:31:31 +0200

    refactor: relocate fetch reques ligic from LikeButton to Post

--- components/LikeButton/LikeButton.tsx
+++ components/LikeButton/LikeButton.tsx
@@ -6,28 +6,18 @@ import styles from './LikeButton.module.css';
 import cn from 'classnames';
 import LikeIcon from '../../public/LikeIcon.svg';
 
-
 export const LikeButton = ({ isLiked, setIsLiked, ...props }: LikeButtonProps): JSX.Element => {
   const handleLike = (): void => {
     setIsLiked(!isLiked);
-
-    fetch('https://jsonplaceholder.typicode.com/posts/1', {
-      method: 'PATCH',
-      body: JSON.stringify({ liked: !isLiked }),
-      headers: {
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-    })
-      .then((response) => response.json())
-      .then((json) => console.log(json));
   };
+
   return (
     <button
       className={cn(styles.like, { [styles.liked]: isLiked })}
       onClick={handleLike}
       {...props}
     >
-      <LikeIcon/>
+      <LikeIcon />
     </button>
   );
 };
--- components/Post/Post.tsx
+++ components/Post/Post.tsx
@@ -1,10 +1,22 @@
 'use client';
 
-import { useState, type JSX } from 'react';
+import { useEffect, useState, type JSX } from 'react';
 import { LikeButton } from '../LikeButton/LikeButton';
 
 export const Post = (): JSX.Element => {
   const [isLiked, setIsLiked] = useState<boolean>(false);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts/1', {
+      method: 'PATCH',
+      body: JSON.stringify({ liked: !isLiked }),
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, [isLiked]);
   return (
     <article>
       <LikeButton isLiked={isLiked} setIsLiked={setIsLiked} />
